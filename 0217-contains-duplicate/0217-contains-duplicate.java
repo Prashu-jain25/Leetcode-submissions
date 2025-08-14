0d693bc43295571fdf0ff1b601143ce2ff69ea02
Priyanshu Jain <priyanshujain.tech@gmail.com>
2025-08-14 21:51:04 +0530

    Time: 33 ms (5.12%), Space: 61.4 MB (6.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,6 +1,30 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        return sol_1(nums);
+        // return sol_1(nums);
+        return sol_2(nums);
+    }
+
+    private boolean sol_2(int[] nums) {
+        int n = nums.length;
+
+        if(n == 1) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            mpp.put(nums[i], mpp.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(int x : mpp.values()) {
+            if(x >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+
     }
 
     private boolean sol_1(int[] nums) {
