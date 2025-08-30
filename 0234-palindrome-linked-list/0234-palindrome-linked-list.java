4fc45559d82af62f5ce0939fa28615f37dfc385a
Priyanshu Jain <priyanshujain.tech@gmail.com>
2025-08-30 18:41:31 +0530

    Time: 5 ms (54.76%), Space: 68.2 MB (54.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,6 +10,61 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        // return bruteForce(head);
+        return optimal(head);
+    }
+
+    private boolean optimal(ListNode head) {
+        if(head == null || head.next == null) {
+            return true;
+        }
+
+        // finding mid
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // reversing secondHalf of LL
+        ListNode secondHalf = reverse(slow);
+
+        ListNode firstHalf = head;
+        ListNode temp = secondHalf;
+
+        // Check for palindrome
+        while(secondHalf != null) {
+            if(secondHalf.val != firstHalf.val) {
+                return false;
+            }
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        reverse(temp); // restoring list in it's original form
+
+        return true;
+
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr != null) {
+            ListNode front = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = front;
+        }
+
+        return prev;
+    } 
+
+    private boolean bruteForce(ListNode head) {
         Stack<Integer> st = new Stack<>();
         ListNode temp = head;
 
