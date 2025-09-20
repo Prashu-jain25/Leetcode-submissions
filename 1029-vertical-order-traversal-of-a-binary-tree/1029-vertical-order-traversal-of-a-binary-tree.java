f0abfa67b65e55b79cb2bb1e79b25fc0ecb08145
Priyanshu Jain <priyanshujain.tech@gmail.com>
2025-09-20 13:01:48 +0530

    Time: 3 ms (80.06%), Space: 42.7 MB (51.12%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.java
@@ -14,66 +14,64 @@
  * }
  */
 class Solution {
-    public class Pair{
-        TreeNode node;
-        int row; 
+    private class Pair {
         int col;
+        int row;
+        TreeNode node;
 
         public Pair(TreeNode node, int row, int col) {
             this.node = node;
             this.row = row;
             this.col = col;
-        }
+        } 
     }
-
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        if(root == null) {
-            return ans;
-        }
-
         TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> mpp = new TreeMap<>();
-        Queue<Pair> q = new LinkedList<>();
 
+        Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(root, 0, 0));
 
         while(!q.isEmpty()) {
             Pair curr = q.poll();
-            TreeNode currNode = curr.node;
-            int currRow = curr.row;
-            int currCol = curr.col;
+            TreeNode node = curr.node;
+            int row = curr.row;
+            int col = curr.col;
 
-            if(!mpp.containsKey(currCol)) {
-                mpp.put(currCol, new TreeMap<>());
+            if(!mpp.containsKey(col)) {
+                mpp.put(col, new TreeMap<>());
+            } 
+
+            if(!mpp.get(col).containsKey(row)) {
+                mpp.get(col).put(row, new PriorityQueue<>());
             }
 
-            if(!mpp.get(currCol).containsKey(currRow)) {
-                mpp.get(currCol).put(currRow, new PriorityQueue<>());
+            mpp.get(col).get(row).add(node.val);
+
+            if(node.left != null) {
+                q.add(new Pair(node.left, row + 1, col - 1));
             }
 
-            mpp.get(currCol).get(currRow).add(currNode.val);
-
-            if(currNode.left != null) {
-                q.add(new Pair(currNode.left, currRow + 1, currCol - 1));
+            if(node.right != null) {
+                q.add(new Pair(node.right, row + 1, col + 1));
             }
 
-            if(currNode.right != null) {
-                q.add(new Pair(currNode.right, currRow + 1, currCol + 1));
-            }
         }
 
+        List<List<Integer>> res = new ArrayList<>();
+
         for(TreeMap<Integer, PriorityQueue<Integer>> rows : mpp.values()) {
-            List<Integer> colList = new ArrayList<>();
+            List<Integer> temp = new ArrayList<>();
+
             for(PriorityQueue<Integer> pq : rows.values()) {
                 while(!pq.isEmpty()) {
-                    colList.add(pq.poll());
+                    temp.add(pq.poll());
                 }
             }
 
-            ans.add(colList);
+            res.add(temp);
         }
 
-        return ans;
+        return res;
+
     }
 }
