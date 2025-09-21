24570dfc2dbd206cc7dbcfec501474df2b9fdfae
Priyanshu Jain <priyanshujain.tech@gmail.com>
2025-09-21 14:09:10 +0530

    Time: 5 ms (88.87%), Space: 45 MB (31.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,90 +1,24 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-   
-            // int n = s.length();
-            // int i = 0, j = 0, maxLen = 0;
-            // HashMap<Character, Integer> mpp = new HashMap<>();
+        int len = s.length();
 
-            // while(j < n) {
-            //     char ch = s.charAt(j);
-            //     mpp.put(ch, mpp.getOrDefault(ch, 0) + 1);
-
-            //     if(mpp.get(ch) == 1) {
-            //         maxLen = Math.max(maxLen, j - i + 1);
-            //     }
-            //     else if(mpp.get(ch) > 1) {
-            //         while(mpp.get(ch) > 1) {
-            //             char del = s.charAt(i);
-            //             mpp.put(del, mpp.get(del) - 1);
-
-            //             if(mpp.get(del) == 0) {
-            //                 mpp.remove(del);
-            //             }
-
-            //             i++;
-            //         }
-            //     }
-
-            //     j++;
-            // }
-
-            // return maxLen;
-
-            // return brute(s);
-            return optimal(s);
-        
-    }
-
-    private int optimal(String s) {
-        int n = s.length();
-        int left = 0, right = 0, maxi = 0;
-        HashSet<Character> set = new HashSet<>();
-
-        while(right < n) {
-            char ch = s.charAt(right);
-            if(!set.contains(ch)) {
-                set.add(ch);
-                maxi = Math.max(maxi, right - left + 1);
-                right++;
-            }
-            else{
-                set.remove(s.charAt(left));
-                left++;
-            }
-        }
-
-        return maxi;
-    }
-
-
-    private int brute(String s) {
-        int n = s.length();
+        int start = 0, end = 0;
         int maxi = 0;
 
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(allUnique(s, i, j)) {
-                    maxi = Math.max(maxi, j - i + 1);
-                }
+        HashMap<Character, Integer> mpp = new HashMap<>();
+
+        while(end < len) {
+            char ch = s.charAt(end);
+
+            if(mpp.containsKey(ch)) {
+                start = Math.max(start, mpp.get(ch) + 1) ;
             }
+
+            mpp.put(ch, end);
+            maxi = Math.max(maxi, end - start + 1);
+            end++;
         }
 
         return maxi;
-    }
-
-    private boolean allUnique(String s, int i, int j) {
-        HashSet<Character> set = new HashSet<>();
-
-        for(int k = i; k <= j; k++) {
-            char ch = s.charAt(k);
-
-            if(set.contains(ch)) {
-                return false;
-            }
-
-            set.add(ch);
-        }
-
-        return true;
     }
 }
