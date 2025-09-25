bdfd8a45eb8c2f1fa1942e03823fceb988afce53
Priyanshu Jain <priyanshujain.tech@gmail.com>
2025-09-25 07:27:37 +0530

    Time: 0 ms (100%), Space: 41.6 MB (29.67%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,6 +10,34 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
+        // return swapData(head);
+        return swapNodes(head);
+    }
+
+    private ListNode swapNodes(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        while(prev.next != null && prev.next.next != null) {
+            ListNode first = prev.next;
+            ListNode second = prev.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            prev = first;
+        }
+
+        return dummy.next;
+    }
+
+
+    private ListNode swapData(ListNode head) {
         if(head == null || head.next == null) {
             return head;
         }
@@ -17,10 +45,10 @@ class Solution {
         ListNode curr = head;
 
         while(curr != null && curr.next != null) {
-            int temp = curr.val;
-            curr.val = curr.next.val;
-            curr.next.val = temp;
-            curr = curr.next.next;
+            int temp = curr.val; // copy curr data in a variable
+            curr.val = curr.next.val; // putting next's data in curr
+            curr.next.val = temp; // putting temp into next node's data
+            curr = curr.next.next;// moving curr twice
         }
 
         return head;
